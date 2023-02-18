6becf4f459c473822542103e5620c41c2fd23976
ayesha <zahraayesha414@gmail.com>
2023-02-18 10:56:08 +0500

    mobile styling

--- src/App.js
+++ src/App.js
@@ -4,96 +4,24 @@ import { useState } from 'react';
 import {v4 as uuidv4} from 'uuid';
 import AddEmployee from './components/AddEmployee';
 import EditEmployee from './components/EditEmployee';
+import Header from './components/Header';
+import Employees from './Pages/Employees';
+import {BrowserRouter, Routes, Route} from 'react-router-dom'; 
+import Customers from './Pages/Customers';
+
 
 function App() {
-  const [role, setRole] = useState('GIS Analyst');
-  const [employees, setEmployees] = useState(
-    [
-      { id: 1,
-        name: 'Ayesha', 
-      role: 'Web Developer', 
-      img: 'https://images.pexels.com/photos/3586798/pexels-photo-3586798.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
-    },
-    { id: 2,
-      name: 'John', 
-      role: 'Front end Developer', 
-      img: 'https://images.pexels.com/photos/694438/pexels-photo-694438.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
-    },
-    {
-      id: 3,
-      name: 'Caleb', 
-      role: 'Back end Developer', 
-      img: 'https://images.pexels.com/photos/775358/pexels-photo-775358.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
-    },
-    { id: 4,
-      name: 'Elsa', 
-      role: 'Engineer', 
-      img: 'https://images.pexels.com/photos/3610877/pexels-photo-3610877.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
-    },
-    ]);
-
-    function updateEmployee(id, newName, newRole){
-      const updatedEmployees = employees.map((employee)=>{
-        if (id == employee.id){
-
-        return {...employee, name: newName, role: newRole};
-        }
-        return employee;
-      });
-      setEmployees(updatedEmployees);
-    }
-    
-    function newEmployee(name, role, img) {
-      const newEmployee = {
-        id: uuidv4(),
-        name: name,
-        role: role,
-        img: img,
-      };
-      setEmployees( [...employees, newEmployee]
-
-      )
-    }
-
-  const showEmployees = true;
-  return (
-    <div className="App">
-      {
-      console.log('inside the return') }
-      
-      {showEmployees ? (
-        <>
-        <input type= 'text' onChange={(e) =>{
-          setRole(e.target.value);
-        }}/>
-        <div 
-        className= "flex flex-wrap justify-center">
-       {employees.map((employee) => {
-        const editEmployee = (
-        <EditEmployee id={employee.id} 
-        name={employee.name} 
-        role={employee.role} 
-        updateEmployee={employee.updateEmployee}/>
-       );
-        return (
-          <Employee
-          key= {employee.id}
-          id= {employee.id}
-          name= {employee.name}
-          role= {employee.role}
-          img= {employee.img}
-          editEmployee={editEmployee}
-          />
-        );
-       })}
-        </div>
-        <AddEmployee newEmployee = {newEmployee}/>
-        </>
-       ) : (
-        <p>You cannot see the Employees</p>
-       )}
-    </div>
-  );
+     return (
+     
+        <BrowserRouter>
+         <Header>
+        <Routes>
+          <Route path= "/employees"element={<Employees/>}/>
+          <Route path= "/customers"element={<Customers/>}/>
+        </Routes>
+        </Header>
+        </BrowserRouter>     
+     );
 }
 
 export default App;
--- src/components/Employee.js
+++ src/components/Employee.js
@@ -2,7 +2,8 @@ import EditEmployee from "./EditEmployee";
 
 function Employee(props) {
     return (
-        <div className="min-w-[350px] max-w-[350px] py-8 px-8 max-w-sm bg-white rounded-xl shadow-lg space-y-2 sm:py-4 sm:flex sm:items-center sm:space-y-0 sm:space-x-6">
+        <div className="min-w-[350px] max-w-[350px] py-8 px-8 max-w-sm bg-white rounded-xl 
+        shadow-lg space-y-2 sm:py-4 sm:flex sm:items-center sm:space-y-0 sm:space-x-6 my-2 mx-2">
   <img className="block mx-auto h-24 rounded-full sm:mx-0 sm:shrink-0" 
   src={props.img} 
   alt="Woman's Face"/>
